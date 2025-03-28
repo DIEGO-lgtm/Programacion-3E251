e43200aa7ef9e5d848bbadbaeb6ff0a3508e2c8a
DIEGO-lgtm <a24110241@ceti.mx>
2025-03-28 03:30:16 +0000

    Practica 2

--- Programacion/main.cpp
+++ Programacion/main.cpp
@@ -1,29 +1,27 @@
 #include "Serie.hpp"
-
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <iostream>
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+  auto pantalla = Screen::Create(
+    ftxui::Dimension::Full(),
+    Dimension::Full()
 
-Serie serienavidena;
+  );
 
-serienavidena.ApagarTodo();
-serienavidena.Imprimir();
-serienavidena.EncenderTodo();
-serienavidena.Imprimir();
-serienavidena.ApagarCantidad(8);
-serienavidena.Imprimir();
-serienavidena.EncenderCantidad(3);
-serienavidena.Imprimir();
-serienavidena.AlternarEncendido();
-serienavidena.Imprimir();
-serienavidena.RecorrerDerecha();
-serienavidena.Imprimir();
-serienavidena.RecorrerIzquierda();
-serienavidena.Imprimir();
+  auto documento = vbox(
+    spinner(21,1)
+  );
+
+  Render(pantalla, documento);
+  pantalla.Print();
 
 
-
-  return 0;
 }
 
 //g++ main.cpp -o programa && ./programa
+//c++ main.cpp -o programa -lftxui-component -lftxui-dom -lftxui-screen -std=c++17
